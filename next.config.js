b54ddfda92e70a9ad03665773687bab34ca12066
mchlsttz <michael.stutz@sasis.ch>
2025-12-16 13:38:14 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    // Verhindert, dass ESLint Errors (z. B. no-unused-vars) den Build auf Vercel abbrechen
-    ignoreDuringBuilds: true,
-  },
-};
 
-module.exports = nextConfig;
